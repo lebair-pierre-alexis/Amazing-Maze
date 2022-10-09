a6a556909f81804356e30e9b36450e0e1ec81c74
lebair-pierre-alexis <pierre-alexis.lebair@laplateforme.io>
2022-10-10 00:01:53 +0200

    Generation part done

--- amazingmaze/common/maze.py
+++ amazingmaze/common/maze.py
@@ -1,119 +1,125 @@
+from math import sqrt
 import numpy as np
 import random
 from amazingmaze.common import file
 
+
+class   Cell():
+
+    wall_pairs = {'N': 'S', 'S': 'N', 'E': 'W', 'W': 'E'}
+
+    def __init__(self, x, y) -> None:
+        self._x = x
+        self._y = y
+        self.walls = {'N': True, 'S': True, 'E': True, 'W': True}
+
+    def hasWalls(self):
+        return all(self.walls.values())
+
+    def breakWall(self, other, wall):
+        self.walls[wall] = False
+        other.walls[Cell.wall_pairs[wall]] = False
+
 class   Maze():
-    
-    _dic = {
-        "N": (-1, 0),
-        "S": (1, 0),
-        "E": (0, 1),
-        "W": (0, -1),
-    }
 
-    def __init__(self, fileName = "") -> None:
-        self._map = None
-        self._size = -1
+    delta = [('W', (0, -1)),
+             ('E', (0, 1)),
+             ('S', (1, 0)),
+             ('N', (-1, 0))]
+
+    def __init__(self, size = 0, fileName = "") -> None:
         self._fileName = fileName
-
-        if fileName == "":
-            self._createMap()
-            self._gen(0, 0)
-            self._cleanMap()
+        if size != -1:
+            self._map = [[Cell(x, y) for y in range(size)] for x in range(size)]
+            self._size = size * size
         else:
-            self._loadMap()
+            self.loadMap()
 
-    def _cleanMap(self, exeption = ""):
-        for x in range(self._size * 2 - 1):
-            for y in range(self._size * 2 - 1):
-                if self._map[x][y] != '.' and self._map[x][y] != '#' and self._map[x][y] != exeption:
-                    self._map[x][y] = '.'
+    def _cellAt(self, x, y):
+        return self._map[x][y]
 
-    def _gen(self, posx, posy):
-        self._map[posx][posy] = 'V'
-        while True:
-            dir = self._hasNeighbors(posx, posy)
-            lenght = len(dir)
-            if lenght == 0:
-                break
-            toBreak = dir.pop(dir.index(random.choice(dir)))
-            self._map[posx + self._dic[toBreak][0]][posy + self._dic[toBreak][1]] = '.'
-            if lenght == 1:
-                posx += self._dic[toBreak][0] * 2
-                posy += self._dic[toBreak][1] * 2
-                self._map[posx][posy] = 'V'
-            else:
-                self._gen(posx + self._dic[toBreak][0] * 2, posy + self._dic[toBreak][1] * 2)
+    def _findValidNeighbour(self, cell:Cell):
+        neighbours = []
+        for direction, (dx, dy) in self.delta:
+            x2, y2 = cell._x + dx, cell._y + dy
+            if 0 <= x2 < int(sqrt(self._size)) and 0 <= y2 < int(sqrt(self._size)):
+                neighbour = self._cellAt(x2, y2)
+                if neighbour.hasWalls():
+                    neighbours.append((direction, neighbour))
+        return neighbours
 
-    def _hasNeighbors(self, posx, posy):
-        result = []
-        for dir in self._dic.keys():
-            if 0 <= posx + self._dic[dir][0] < self._size * 2 - 1 and 0 <= posy + self._dic[dir][1] < self._size * 2 - 1:
-                if self._map[posx + self._dic[dir][0] * 2][posy + self._dic[dir][1] * 2] == '.':
-                    result.append(dir)
-        return result
+    def gen(self, x, y):
+        cellStack = []
+        currentCell = self._cellAt(x, y)
+        visited = 1
+        size = int(sqrt(self._size))
 
-    def _mapFromText(self, content, size):
-        x, posx, posy = 0, 0, 0
-        length = len(content)
-        while content[x] != '\n':
-            x += 1
-        x += 2
-        for posx in range(size):
-            for posy in range(size):
-                self._map[posx][posy] = content[x]
-                if content[x + 2] == '\n':
-                    if content[x + 1] == '.':
-                        break
-                    else:
-                        x += 4
-                else:
-                    x += 1
+        while visited < self._size:
+            neighbours = self._findValidNeighbour(currentCell)
+            if not neighbours:
+                currentCell = cellStack.pop()
+                continue
+            direction, nextCell = random.choice(neighbours)
+            currentCell.breakWall(nextCell, direction)
+            cellStack.append(currentCell)
+            currentCell = nextCell
+            visited += 1
+        self._map[size - 1][size - 1].walls["E"] = False
+        self._map[0][0].walls["W"] = False
 
-    def _loadMap(self):
+    def loadMap(self):
         content = file.readFile(self._fileName)
-        for x in range(len(content)):
-            if content[x] == "\n":
+        for size, x in enumerate(content):
+            if x == '\n':
                 break
-        self._size = x
-        self._size = int((self._size - 1) / 2)
-        size = self._size + self._size - 1
-        self._map = np.zeros((size, size), dtype=str)
-        np.set_printoptions(threshold=np.inf)
-        self._mapFromText(content, size)
+        self._size = int(((size - 1) / 2)**2)
+        self._map = [[Cell(x, y) for y in range(int(sqrt(self._size)))] for x in range(int(sqrt(self._size)))]
+        content = content.splitlines()
+        for x, line in enumerate(content):
+            if x and x != len(line) - 1:
+                for y, l in enumerate(line):
+                    if y != 0 and y != len(line) - 1:
+                        if x % 2:
+                            if y % 2 == 0 and l == ".":
+                                self._map[int(x / 2)][int(y / 2) - 1].walls["E"] = False
+                                self._map[int(x / 2)][int(y / 2)].walls["W"] = False
+                        else:
+                            if y % 2 and l == ".":
+                                self._map[int(x / 2) - 1][int(y / 2)].walls["S"] = False
+                                self._map[int(x / 2)][int(y / 2)].walls["N"] = False
 
-    def _mapFromSize(self, size):
-        self._map = np.zeros((size, size), dtype=str)
-        np.set_printoptions(threshold=np.inf)
-        for x in range(size):
-            for y in range(size):
-                if x % 2 != 0 or y % 2 != 0:
-                    self._map[x][y] = "#"
-                else:
-                    self._map[x][y] = "."
-
-    def _createMap(self):
-        self._size = input("Please entre the size of the maze : ")
-        while self._size.isdigit() == False:
-            self._size = input("The size must be a number, please enter a valid size : ")
-        self._size = int(self._size)
-        self._mapFromSize(self._size + self._size - 1)
-    
     def _mapToText(self):
-        mapText = "." + "".join(("#" for x in range(self._size + self._size))) + "\n"
-        for x in range(self._size + self._size - 1):
-            if x == 0:
-                mapText += "." + str(self._map[x])[1:-1].replace("'", "").replace(" ", "").replace("\n", "") + "#\n"
-            elif x == self._size + self._size - 2:
-                mapText += "#" + str(self._map[x])[1:-1].replace("'", "").replace(" ", "").replace("\n", "") + ".\n"
+        size = int(sqrt(self._size))
+        mapText = "." + "".join(("#" for x in range(size * 2))) + "\n"
+        for x, line in enumerate(self._map):
+            if x:
+                mapText += '#'
             else:
-                mapText += "#" + str(self._map[x])[1:-1].replace("'", "").replace(" ", "").replace("\n", "") + "#\n"
-        mapText += "".join("#" for x in range(self._size + self._size)) + "."
+                mapText += '.'
+            wall = ""
+            if x != int(sqrt(self._size)) - 1:
+                wall += "\n#"
+            for y, cell in enumerate(line):
+                mapText += '.'
+                if cell.walls['E'] and (x != int(sqrt(self._size)) - 1 or y != int(sqrt(self._size)) - 1):
+                    mapText += '#'
+                else:
+                    mapText += '.'
+                if x != int(sqrt(self._size)) - 1:
+                    if cell.walls['S']:
+                        wall += "##"
+                    else:
+                        wall += ".#"
+            mapText += wall + '\n'
+        mapText += "".join("#" for x in range(size * 2)) + "."
         return mapText
+
 
     def mapToFile(self):
         content = self._mapToText()
-        file.writeFile(self._fileName, content, "wt")
+        if self._fileName == "":
+            fileName = input("Entrez le nom du fichier dans lequel enregistrer le labyrinthe : ")
+        file.writeFile(fileName, content, "wt")
 
     def printMap(self):
         print(self._mapToText())
--- amazingmaze/main.py
+++ amazingmaze/main.py
@@ -1,9 +1,29 @@
-import sys
+import random
 from amazingmaze.common import maze
 
-sys.setrecursionlimit(10**5)
-
 def run():
-    map = maze.Maze()
+
+    fileName = ""
+
+    while True:
+        try:
+            size = int(input("Enter the size of the maze (enter -1 in case you want to load a existing maze) : "))
+        except ValueError:
+            print("Enter a valide number (either -1 or a positive number")
+            continue
+        if size != -1 and size < 0:
+            print("Enter a valide number (either -1 or a positive number")
+            continue
+        elif size == -1:
+            fileName = input("You choose to open a existing maze, please enter it's name :")
+            break
+        else:
+            break
+
+    map = maze.Maze(size, fileName)
+    #map.loadMap()
+    if fileName == "":
+        x, y = random.randrange(0, size), random.randrange(0, size)
+        map.gen(0, 0)
     map.printMap()
     map.mapToFile()
